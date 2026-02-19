9d7b4db63d2426f94bc245828e36a8e924994fad
yevdjx <iulechklimkin@yandex.ru>
2026-02-20 02:20:24 +0500

    Закомментила код-пример для нового сервиса

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import uvicorn
-
-app = FastAPI()
-
-# Подключаем папку со статическими файлами (CSS)
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Указываем папку с шаблонами (HTML)
-templates = Jinja2Templates(directory="templates")
-
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+# from fastapi import FastAPI, Request
+# from fastapi.responses import HTMLResponse
+# from fastapi.staticfiles import StaticFiles
+# from fastapi.templating import Jinja2Templates
+# import uvicorn
+#
+# app = FastAPI()
+#
+# # Подключаем папку со статическими файлами (CSS)
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+#
+# # Указываем папку с шаблонами (HTML)
+# templates = Jinja2Templates(directory="templates")
+#
+# @app.get("/", response_class=HTMLResponse)
+# async def read_root(request: Request):
+#     return templates.TemplateResponse("index.html", {"request": request})
+#
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
